0d48317001ab770d442c107c0ff6dc3b97792fb6
Markus Malz <markus.malz@it2media.de>
2020-09-18 16:21:10 +0200

    Pass as args

--- src/Catch.ConsoleApp/Program.cs
+++ src/Catch.ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.IO.Abstractions;
 
 namespace Catch.ConsoleApp
@@ -7,10 +9,28 @@ namespace Catch.ConsoleApp
     {
         static void Main(string[] args)
         {
+            var path = args[0];
+            DirectoryInfo di = new DirectoryInfo(path);
+            Console.WriteLine($"Path: {di.FullName}");
+            var searchPattern = args[1];
+            Console.WriteLine($"SearchPattern: {searchPattern}");
+            var searchWords = new List<string>();
+            if (args.Length > 2)
+            {
+                for (var index = 2; index < args.Length; index++)
+                {
+                    var arg = args[index];
+                    searchWords.Add(arg);
+                }
+            }
+            
+            var content = string.Join(' ', searchWords);
+            Console.WriteLine($"Content: {content}");
+            
             IFileSystem fileSystem = new FileSystem();
             var fileSystemReader = new SimpleFileSystemReader(fileSystem);
             var searcher = new Searcher(fileSystemReader);
-            var results = searcher.Search("/home/lordasgart/Nextcloud/Notes", "*.md", "git credentials");
+            var results = searcher.Search(path, searchPattern , content);
 
             foreach (var result in results)
             {
